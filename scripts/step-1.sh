f802d931ee038d3b143c577df1004bbfb66110fd
PiercingXX <155384428+PiercingXX@users.noreply.github.com>
2026-04-20 13:32:11 -0400

    Update step-1.sh

--- scripts/step-1.sh
+++ scripts/step-1.sh
@@ -132,7 +132,7 @@ ensure_tty_boot_without_gdm() {
     sudo pacman -S tmux --noconfirm
     sudo pacman -S sshpass --noconfirm
     sudo pacman -S htop --noconfirm
-    sudo pacman -S gnome-shell --noconfirm
+    sudo pacman -S gnome --noconfirm
     sudo pacman -S --needed --noconfirm webkit2gtk-4.1
     install_optional_arch_packages linux-firmware-brcm43752 linux-firmware-broadcom linux-firmware-realtek
 
